084da2e5abaef5041a0a4a9703f86da9e484f2aa
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-10-07 00:00:00 +0200

    Cleaned with php-cs.

--- Module.php
+++ Module.php
@@ -1,25 +1,12 @@
-<?php 
-  
+<?php
+
 namespace PdfText;
 
 use Omeka\Module\AbstractModule;
-use Omeka\Module\Manager as ModuleManager;
 use Omeka\Module\Exception\ModuleCannotInstallException;
-use Zend\View\Model\ViewModel;
-use Zend\Mvc\Controller\AbstractController;
-use Zend\Form\Fieldset;
-use Zend\EventManager\Event;
 use Zend\EventManager\SharedEventManagerInterface;
-use Zend\Mvc\MvcEvent;
 use Zend\ServiceManager\ServiceLocatorInterface;
-use Zend\Form\Element\Textarea;
-use Zend\Form\Element\Text;
-use Zend\Debug\Debug;
-use Omeka\Mvc\Controller\Plugin\Logger;
 //use Zend\Log\Logger;
-use Zend\Log\Writer;
-use PdfText\Form\Config as ConfigForm;
-use Zend\View\Renderer\PhpRenderer;
 
 class Module extends AbstractModule
 {
@@ -39,43 +26,42 @@ class Module extends AbstractModule
     {
         return include __DIR__ . '/config/module.config.php';
     }
-        
+
     /**
      * Attach listeners to events.
      *
      * @param SharedEventManagerInterface $sharedEventManager
      */
-
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
         $sharedEventManager->attach(
-          'Omeka\Api\Adapter\MediaAdapter',
-         'api.hydrate.post',
-          function (\Zend\EventManager\Event $event) {
-              $entity = $event->getParam('entity');
-              if (! $entity->getId()) {
-                  $fileExt = $entity->getExtension();
-                  if (in_array($fileExt, array('pdf', 'PDF'))) {
-                      // Path du fichier
-                      $basePath = $this->getServiceLocator()->get('Config')['file_store']['local']['base_path'] ?: (OMEKA_PATH . '/files');
-                      $filePath = $basePath . '/original/' . $entity->getStorageId() . '.' . $fileExt;
-                      $item = $entity->getItem();
-                      $itemId = $item->getId();
-                      $text = $this->pdfToText($filePath);
-                      $apiManager = $this->getServiceLocator()->get('Omeka\ApiManager');
-                      // Update item's bibo:content property
-                      $data = [
-                    "bibo:content" => [[
-                        "type"=> "literal",
-                        "property_id"=> 91,
-                        "@value"=> $text
-                    ]],
-                ];
-                      $response = $apiManager->update('items', $itemId, $data, [], ['isPartial' => true, 'collectionAction' => 'append']);
-                  }
-              }
-          }
-      );
+            'Omeka\Api\Adapter\MediaAdapter',
+            'api.hydrate.post',
+            function (\Zend\EventManager\Event $event) {
+                $entity = $event->getParam('entity');
+                if (! $entity->getId()) {
+                    $fileExt = $entity->getExtension();
+                    if (in_array($fileExt, ['pdf', 'PDF'])) {
+                        // Path du fichier
+                        $basePath = $this->getServiceLocator()->get('Config')['file_store']['local']['base_path'] ?: (OMEKA_PATH . '/files');
+                        $filePath = $basePath . '/original/' . $entity->getStorageId() . '.' . $fileExt;
+                        $item = $entity->getItem();
+                        $itemId = $item->getId();
+                        $text = $this->pdfToText($filePath);
+                        $apiManager = $this->getServiceLocator()->get('Omeka\ApiManager');
+                        // Update item's bibo:content property
+                        $data = [
+                            "bibo:content" => [[
+                                "type" => "literal",
+                                "property_id" => 91,
+                                "@value" => $text,
+                            ]],
+                        ];
+                        $response = $apiManager->update('items', $itemId, $data, [], ['isPartial' => true, 'collectionAction' => 'append']);
+                    }
+                }
+            }
+        );
     }
 
     public function pdfToText($path)
